776b12d5f7d30383c70a6368d80cae410d4f1806
yaredgebre <yaredgebre120@gmail.com>
2026-07-23 14:23:34 -0300

    fillna, rename, groupby, isnull

--- test_pandas.py
+++ test_pandas.py
@@ -1,5 +1,7 @@
 import pandas as pd
 df = pd.read_csv("sample_table.csv")
 df["Salary"] = [1000,1000,1234,1234,4120]
-print(df.drop("Salary", axis=1))
-print(df)
+print(df.isnull())
+print(df.fillna("america"))
+print(df.rename({"Name":"Name2345"}))
+print(df.groupby("ID")["Age"].sum())
